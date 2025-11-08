f37f8579f005292360078c0b7324f4628e2feabb
Im_seungmin <seungmin3199@gmail.com>
2025-11-08 16:08:03 +0900

    SPA 관련 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { GlobalStyle } from './Components/Global.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
